df1271bb87a0bfc31c64443f5be48c66c8e543bd
chenjilin <geometrybase@gmail.com>
2020-06-05 14:00:07 +0800

    first commit

---
+++ libgoctp.go
@@ -0,0 +1,13 @@
+// +build linux,cgo
+
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package goctp6315
+
+/*
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/v6.3.15_20190220_api_tradeapi_se_linux64 -Wl,-rpath,${SRCDIR}/api/v6.3.16_20190305_tradeapi_linux64 -lthostmduserapi6315 -lthosttraderapi6315 -lstdc++
+#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/v6.3.15_20190220_api_tradeapi_se_linux64
+*/
+import "C"
